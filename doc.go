b34f6796eafa5411483c053aa8a6ad1b4516f70e
Shinichi MOTOKI <shinichi-motoki@overruns.org>
2021-08-26 21:04:59 +0900

    Initial commit

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Copyright 2021 Shinichi MOTOKI. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package timet is time package template collection.
+package timet
